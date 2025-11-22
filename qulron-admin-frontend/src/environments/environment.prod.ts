d872ba92e6ef7e1f743875bc96176dbc6a1af3f0
Lutfi Alfawadleh <lutfibk25@gmail.com>
2025-11-22 02:43:50 -0500

    edited backend url

--- qulron-admin-frontend/src/environments/environment.prod.ts
+++ qulron-admin-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://qulron.drinkarizona.com/api',
-  apiUrl: 'http://10.15.109.5:30061/api',
+  apiUrl: 'http://10.15.109.2:30061/api',
 
 };
