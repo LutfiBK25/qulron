cef9db41630f8533b0a667672488814ca46b9937
Lutfi Alfawadleh <lutfibk25@gmail.com>
2025-11-27 11:56:01 -0500

    removed auto wired and added constructor injection

--- qulron-engine/src/main/java/com/qulron/qulron_engine/service/TaskService.java
+++ qulron-engine/src/main/java/com/qulron/qulron_engine/service/TaskService.java
@@ -3,7 +3,6 @@ package com.qulron.qulron_engine.service;
 import com.qulron.qulron_engine.entity.Task;
 import com.qulron.qulron_engine.enums.Status;
 import com.qulron.qulron_engine.repository.TaskRepo;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,8 +10,11 @@ import java.util.List;
 @Service
 public class TaskService {
 
-    @Autowired
-    private TaskRepo taskRepo;
+    private final TaskRepo taskRepo;
+
+    public TaskService(TaskRepo taskRepo) {
+        this.taskRepo = taskRepo;
+    }
 
     public Task getTask(Long loadId) {
         return taskRepo.findTop1ByLoadMaster_IdAndTaskStatusInOrderByIdDesc(loadId, List.of(Status.CREATED, Status.STARTED))
